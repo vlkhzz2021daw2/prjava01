e8e9ee05dcd39a063dc3d03ab2f0743ccd221a45
vlkhzz2021daw2@protonmail.com <VLADISLAV@192.168.1.139>
2020-10-13 15:49:21 +0200

    Versió 0.2 del projecte prjava01

--- src/main/java/prjava01/Main.java
+++ src/main/java/prjava01/Main.java
@@ -24,6 +24,7 @@ public class Main {
                 bw.write("<html>");bw.newLine();
                 bw.write(" <head>");bw.newLine();
                 bw.write(" <title>");bw.newLine();
+                bw.write(" <h1>DAW2 m08uf4pr2</h1>");bw.newLine();
                 bw.write(" Nova p&agrave;gina web");bw.newLine();
                 bw.write(" </title>");bw.newLine();
                 bw.write(" </head>");bw.newLine();
